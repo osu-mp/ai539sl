7e14e9d48e26f2c16792a128d340b63f99257929
Matthew Pacey <paceym@oregonstate.edu>
2024-03-02 09:53:35 -0800

    Still WIP for hw4

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -13,65 +13,92 @@ from scipy.io import loadmat
 
 DEBUG = True
 NUM_SIMULATIONS = 10000
-
+MIN_N = 1
+MAX_N = 4
 # plot_dir = 'plots'
 # os.makedirs(plot_dir, exist_ok=True)
 plot_fname = 'hw4.png'
 
+# various matplot styles; the final is a custom dash pattern (only used if n-range >= 5)
+line_styles = ['-', '--', '-.', ':', (0, (3, 5, 1, 5, 1, 5))]
+
+
+
 def markov(x, n):
-    y_vals = [0.0] * len(x)
-    return y_vals
+    return n / x
 
 def chebyshev(x, n):
+    #y_values = n / x **2
     return n / (x ** 2)
 
+def simulate(n, count=NUM_SIMULATIONS):
+    """
+    Simulate the given number of times:
+    sum(X^2_i) for i = 1 to n
+        each X_i is normal from 0 to 1
+    :param n:
+    :param count:
+    :return:
+    """
+    x_s = np.random.uniform(0, 1, (count, n))
+    x_s_sq = x_s ** 2
+    sums = np.sum(x_s_sq, axis=1)
+    if DEBUG:
+        print(f"Simulating with {n=}, {count=}")
+        print(f"{x_s=}\n{x_s_sq=}\n{sums=}")
+    return sums
 def plot_results():
-    n = 3
-    # x_values = np.linspace(0.01, 4, 400)
-    # n_values = list(range(1, 6))
-    x_vals = np.sum(np.random.uniform(0, 1, (NUM_SIMULATIONS, n)) ** 2, axis=1)
-    x_range = np.linspace(0, 4 * n, 50)
-
-    sim_probs = [np.mean(x_vals >= x) for x in x_range]
-
     plt.figure()
-    # for n in n_values:
-    #     plt.plot(x_values, chebyshev(x_values * n, n), label=f'Chebyshev n={n}')
-    plt.plot(x_range, sim_probs, label="Simulated Probabilities")
 
-    markov_bound = markov(x_range, n=None)
-    plt.plot(x_range, markov_bound, label="Markov")
+    # simulated probs
+    for n, line_style in zip(range(MIN_N, MAX_N + 1), line_styles):
+        x_vals = simulate(n)
+        x_range = np.linspace(0, 4 * n, 50)
+        sim_prob = ([np.mean(x_vals >= x) for x in x_range])
+        plt.plot(x_range, sim_prob, label=f"Sim Probs ({n=})", color='blue', linestyle=line_style)
 
-    for label in ['Chebyshev', 'Chernoff', 'Hoeffding']:
-        val = np.random.random(1)
-        plt.plot(x_range, [val] * len(x_range), label=f"TODO {label}")
+    # markov bounds
+    for n, line_style in zip(range(MIN_N, MAX_N + 1), line_styles):
+        x_range = np.linspace(0.000001, 4 * n, 50)
+        x_vals = n / x_range
+        prob = ([np.mean(x_vals >= x) for x in x_range])
+        plt.plot(x_range, prob, label=f"Markov ({n=})", color='red', linestyle=line_style)
+
+    # chebyshev
+    for n, line_style in zip(range(MIN_N, MAX_N + 1), line_styles):
+        x_range = np.linspace(0.000001, 4 * n, 50)
+        x_vals = n / (x_range ** 2)
+        prob = ([np.mean(x_vals >= x) for x in x_range])
+        plt.plot(x_range, prob, label=f"Chebyshev ({n=})", color='Green', linestyle=line_style)
+
+    # TODO Chernoff
+    # TODO Hoeffding (for a few k values)
+
+    # for n, line_style in zip(range(MIN_N, MAX_N + 1), line_styles):
+    #     x_vals = simulate(n)
+    #     x_range = np.linspace(0, 4 * n, 50)
+    #     sim_probs.append([np.mean(x_vals >= x) for x in x_range])
+    #
+    #     markov_bound = markov(x_range, n=None)
+    #     plt.plot(x_range, markov_bound, label=f"Markov ({n=})", color='red', linestyle=line_style)
+
+
+    # for label in ['Chernoff', 'Hoeffding']:
+    #     val = np.random.random(1)
+    #     plt.plot(x_range, [val] * len(x_range), label=f"TODO {label}")
 
     plt.title('Concentration Inequalities')
     plt.xlabel('x')
     plt.ylabel('P(X >= x)')
-    plt.legend()
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     plt.grid(True)
-    plt.show()
+    plt.tight_layout()
     plt.savefig(plot_fname)
+    plt.show()
+
 def main():
 	plot_results()
 
-def main_old():
-    if RUN_TRIALS:
-        test_buckets = build_test_buckets(X_test, Y_test)
-        params = [(m, n, test_buckets) for m in m_values for n in n_values]
-
-        # each config is independent, so can run in a separate thread
-        # can plot once all are completed
-        with Pool(processes=cpu_count() - 2) as pool:
-            results = pool.map(run_config, params)
-
-        # Save the results to a pickle file
-        with open(results_pkl, 'wb') as file:
-            pickle.dump(results, file)
-
-    plot_results()
-
-
 if __name__ == "__main__":
     main()
+    # simulate(n=6, count=4)
