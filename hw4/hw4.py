b8f2a6818a18949b6683718e7c03a640495f0c29
Matthew Pacey <paceym@oregonstate.edu>
2024-03-03 20:33:49 -0800

    Final updates probably

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -9,25 +9,30 @@ import numpy as np
 import os
 from scipy.stats import chi2
 
-DEBUG = True
 plot_dir = 'plots'
 os.makedirs(plot_dir, exist_ok=True)
 
 # various matplot styles; the final is a custom dash pattern (only used if n-range >= 5)
 line_styles = ['-', '--', '-.', ':', (0, (3, 5, 1, 5, 1, 5))]
 
-def plot_new():
-
+def plot():
+    """
+    Plot concentration inequalities for various bounds on a Chi-square
+    random variable with n degrees of freedom
+    :return:
+    """
+    # produce separate plots for each n
     for n in [1, 2, 4, 8]:
         plt.figure()
 
         # Add vertical line at x=n
         plt.axvline(x=n, color='black', linestyle='--', label='x=n')
 
+        # plot for x from just after 0 to 4n (to avoid possible div by 0)
         x_stop = 4 * n
-        x_range = np.linspace(0.0001, x_stop, 50)
+        x_range = np.linspace(0.0001, x_stop, 100)
 
-        # simulated
+        # simulated/empirical prob of chi-square with n dof
         sim_prob = [1 - chi2.cdf(x, df=n) for x in x_range]
         plt.plot(x_range, sim_prob, label=f"Sim Prob", color='blue')
 
@@ -39,13 +44,14 @@ def plot_new():
         y_cheb = 2 * n / x_range ** 2
         plt.plot(x_range, y_cheb, label=f"Chebyshev", color='green', linestyle='-.')
 
-        # Chernoff bound: e^(-n x^2 /(2n + 2x)
+        # Chernoff bound: e^(-n x^2 /(2n + 2x) (not correct as it falls bellow empirical bound)
         y_cher = np.exp(-n * x_range ** 2 / (2*x_range + 2 * n))
         plt.plot(x_range, y_cher, label=f"Chernoff", color='purple', linestyle=':')
 
         # Hoeffding for multiple k values = P(union A) +  P(not A)
+        # ideal_k = np.sqrt(x - n) / n ** (1/4)
         for i, k in enumerate([2, 4, 16]):
-            y_hoeff = 2 * np.exp(-2 * (x_range - n) ** 2 / (n * 2 * k **2))  + 2 * n * np.exp(- k ** 2 / 2)
+            y_hoeff = 2 * np.exp(-2 * (x_range - n) ** 2 / (n * 2 * k **2)) + 2 * n * np.exp(- k ** 2 / 2)
             plt.plot(x_range, y_hoeff, label=f"Hoeffding ({k=})", color='orange', linestyle=line_styles[i])
 
 
@@ -54,7 +60,7 @@ def plot_new():
         plt.ylabel('P(X >= x)')
         plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
         plt.yscale('log')  # Set logarithmic scale for y-axis
-        plt.ylim(0, 20)  # Set maximum y-value
+        plt.ylim(0.0001, 20)  # Set maximum y-value
         plt.grid(True)
         plt.tight_layout()
         plot_fname = os.path.join(plot_dir, f"n_{n}.png")
@@ -63,4 +69,4 @@ def plot_new():
         plt.show()
 
 if __name__ == "__main__":
-    plot_new()
+    plot()
